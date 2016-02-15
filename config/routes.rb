da35c9300a64438bf291413097fd7727743045a3
Scott Werwath <swerwath@berkeley.edu>
2016-02-14 20:58:23 -0800

    Server no longer errors on start up

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   root 'pages#home'
+  get 'stringify', to: 'pages#stringify'
   get 'age', to: 'pages#age'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
