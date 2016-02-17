e1ed62b59756d6c4983c07b6a6a38d0e613753f4
Scott Werwath <swerwath@berkeley.edu>
2016-02-17 08:58:55 -0800

    Finished Q2

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,13 @@
 class Foobar
-  # ENTER CODE FOR Q2 HERE
+  attr_accessor :cat
+  attr_accessor :sat
+  attr_accessor :dat
+
+  def initialize str
+    @dat = str
+  end
+
+  def bar arg, sat: "", dat: ""
+    "#{ arg.to_s }#{@dat}#{sat.to_s}"
+  end
 end
